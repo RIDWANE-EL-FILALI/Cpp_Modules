c9eaaedd0d4e79e63cd301e1df272bf54bd63ad3
B.R.O.L.Y <114589218+RIDWANE-EL-FILALI@users.noreply.github.com>
2023-09-23 18:07:25 +0100

    added the files inside day02

---
+++ day02/ex02/Fixed.hpp
@@ -0,0 +1,46 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Fixed {
+    private:
+        int value;
+        static const int fractionBits = 8;
+    public:
+        Fixed(void);
+        Fixed(Fixed const &src);
+        ~Fixed(void);
+        Fixed(int const n);
+        Fixed(float const f);        
+
+        Fixed& operator=(Fixed const &src);
+
+        bool operator>(Fixed const & src);
+        bool operator<(Fixed const & src);
+        bool operator>=(Fixed const & src);
+        bool operator<=(Fixed const & src);
+        bool operator==(Fixed const & src);
+        bool operator!=(Fixed const & src);
+
+        Fixed operator+(Fixed const & src);
+        Fixed operator*(Fixed const & src);
+        Fixed operator-(Fixed const & src);
+        Fixed operator/(Fixed const & src);
+
+        Fixed & operator++(void);
+        Fixed operator++(int);
+        Fixed & operator--(void);
+        Fixed operator--(int);
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void)const;
+        int toInt(void)const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& number);
+
+#endif
