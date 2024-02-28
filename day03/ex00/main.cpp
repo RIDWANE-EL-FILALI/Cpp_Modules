dbe48e77e14583c47ffd34e6e60822e7369db588
B.R.O.L.Y <114589218+RIDWANE-EL-FILALI@users.noreply.github.com>
2024-02-28 14:50:06 +0100

    added all the exercises of the day08

--- day03/ex00/main.cpp
+++ day03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rel-fila <rel-fila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:50:54 by rel-fila          #+#    #+#             */
-/*   Updated: 2023/10/08 14:50:55 by rel-fila         ###   ########.fr       */
+/*   Updated: 2024/02/28 00:04:15 by rel-fila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(void) {
 	ClapTrap	trap0(trapName0);
 	ClapTrap	trap1(trapName1);
 
+    for (int i = 0; i <= 10; i++)
+    {
 	trap0.attack(trapName1);
     trap1.takeDamage(5);
     trap1.beRepaired(5);
@@ -29,5 +31,6 @@ int	main(void) {
     trap1.takeDamage(9);
     trap1.beRepaired(9);
     std::cout << "\e[92mtrap1 for the win ha ha ha, aliens always win \e[0m👽" << std::endl;
-	return (0);
+    }
+    return (0);
 }
